c9a13f8adb48031b18c72a1a4fb2ee2e62fe8c53
zfordahl <166028120+zfordahl@users.noreply.github.com>
2024-09-26 12:45:54 -0500

    Update signout.php
    Added comments

--- Application Template/signout.php
+++ Application Template/signout.php
@@ -1,4 +1,26 @@
-// signout.php
+<!--
+ * Creator: Zach Fordahl
+ * Date: 9/26/2024
+ * Class: CSC450
+ * Instructor: James Tucker
+ * Group: 3
+ * 
+ * Project requirements:
+ * - Desktop or Web application (with optional Android app)
+ * - User authentication and user management
+ * - Messaging (not external email)
+ * - User Profile
+ * - Dashboard
+ * - Some form of transaction between users
+ * - Use a database with at least five tables
+ * 
+ * Description: We are creating a professional application similar to Reddit where working professionals can connect with each other.
+-->
+
+<!-- Developer: [Your Name], 
+     Changes made: [Description], 
+     Date: [YYYY-MM-DD] -->
+
 <?php
 session_start();
 session_destroy();
