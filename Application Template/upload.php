cd17ab175989964da05889f4c96a62d4da2e9df3
zfordahl <166028120+zfordahl@users.noreply.github.com>
2024-09-26 12:46:49 -0500

    Update upload.php
    Added comments

--- Application Template/upload.php
+++ Application Template/upload.php
@@ -1,3 +1,27 @@
+<!--
+ * Creator: Zach Fordahl
+ * Date: 9/26/2024
+ * Class: CSC450
+ * Instructor: James Tucker
+ * Group: 3
+ * 
+ * Project requirements:
+ * - Desktop or Web application (with optional Android app)
+ * - User authentication and user management
+ * - Messaging (not external email)
+ * - User Profile
+ * - Dashboard
+ * - Some form of transaction between users
+ * - Use a database with at least five tables
+ * 
+ * Description: We are creating a professional application similar to Reddit where working professionals can connect with each other.
+-->
+
+<!-- Developer: [Your Name], 
+     Changes made: [Description], 
+     Date: [YYYY-MM-DD] -->
+
+
 <?php
 session_start();
 include('databaseConnection.php');
@@ -25,4 +49,6 @@ if (isset($_FILES['profileImage']) && $_FILES['profileImage']['error'] === UPLOA
 }
 
 echo json_encode($response);
+
+
 
